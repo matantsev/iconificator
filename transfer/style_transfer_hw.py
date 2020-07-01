1d2f72c82c247091ab9d06f0da5061e3e87e35d8
Andrey <andrey.alexandrow@uptick.com>
2020-07-01 18:43:02 +0300

    wip

---
+++ transfer/style_transfer_hw.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+import torch
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+from transfer.model.global_props import device
+
+# desired size of the output image
+imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
+loader = transforms.Compose([
+    transforms.Resize(imsize),  # scale imported image
+    transforms.ToTensor()])  # transform it into a torch tensor
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)
+    # fake batch dimension required to fit network's input dimensions
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+style_img = image_loader("./transfer/images/picasso.jpg")
+
+content_img = image_loader("./transfer/images/dancing.jpg")
+
+assert style_img.size() == content_img.size(), \
+    "we need to import style and content images of the same size"
+
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
+plt.ion()
+
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)      # remove the fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001) # pause a bit so that plots are updated
+
+
+plt.figure()
+imshow(style_img, title='Style Image')
+
+plt.figure()
+imshow(content_img, title='Content Image')
+
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+input_img = content_img.clone()
+plt.figure()
